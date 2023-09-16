7645a26091babdf264371ddabb0d1bd81c880d29
Starinin Andrey <starininandrey@gmail.com>
2023-09-16 16:21:54 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AccountsManagerApp.BLL;
+using AccountsManagerApp.DAL;
+using AccountsManagerApp.VL;
+
+AccountsManager manager;
+try
+{
+    manager = new AccountsManager(new JsonContext("accounts.json"));
+}
+catch (ArgumentNullException e)
+{
+    CLI.PrintError("Передан пустой путь к файлу");
+    return;
+}
+catch (ArgumentException e)
+{
+    CLI.PrintError("Увзанный файл не существует");
+    return;
+}
+
+var login = CLI.InputString("Введите логин: ");
+var password = CLI.InputString("Введите пароль: ");
+
+var account = manager.Auth(login, password);
+if (account is null)
+{
+    CLI.PrintWarning("Нет такого пользователя!");
+}
+else
+{
+    CLI.PrintSuccess("Успешно авторизовались!");
+}
